032af74d6332e59dbeb7ec530cd2e377d58dca31
Matus Kovacik <mkovacik3@dxc.com>
2020-08-18 15:32:57 +0200

    working loop

--- namedTuple.py
+++ namedTuple.py
@@ -1,11 +1,19 @@
 # Import the 'namedtuple' function from the 'collections' module
 from collections import namedtuple
 # Set the tuple
-individual = namedtuple("Individual", "name age height")
+#tuple je v podstate object
+individual = namedtuple("Nazov_identifikacia", "name age height")
 user = individual(name="Homer", age=37, height=178)
+user2 = individual(name="Peter", age=33, height=175)
 # Print the tuple
 print(user)
+print(user2)
+people = (user, user2)
 # Print each item in the tuple
-print(user.name)
-print(user.age)
-print(user.height)
+#looping through tuple of tuples
+#v pythone loopujes inak, ked das for item in (collection of items), tak “item” bude uz ten tuple, 
+# takze ak chces jeho .name tak das item.name a nie collection[item].
+for i in people:
+  print(i.name)
+  print(i.age)
+  print(i.height)
